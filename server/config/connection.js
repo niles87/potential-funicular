df49e9ccf2cae4e0a4f80b1b02c52b86654c49c1
Niles87 <nbing19@gmail.com>
2021-04-09 21:23:23 -0700

    fixed some errors preventing app from starting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
